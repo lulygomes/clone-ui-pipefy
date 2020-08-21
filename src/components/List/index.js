6061ae7f54a20c5bf3fb4d125fd4a71637c23410
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-08-21 13:46:40 -0300

    Estilização dos componentes

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-// import { Container } from './styles';
+import { MdAdd } from 'react-icons/md';
+
+import Card from '../Card'
+
+import { Container } from './styles';
 
 export default function List() {
   return (
-    <div />
+    <Container >
+      <header>
+        <h2>Tarefas</h2>
+        <button type="button">
+          <MdAdd size={24} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Card />
+        <Card />
+        <Card />
+      </ul>
+    </Container>
   )
 }
